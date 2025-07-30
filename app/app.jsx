c6608b4242205c17710387b88aa75ee1b8eba976
Allan Osei <allanunit15@gmail.com>
2025-07-30 00:21:29 +0000

    navigation

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,9 @@ import HomeScreen from './index';
 import RegisterScreen from './register';
 import LoginScreen from './login';
 import OnboardingScreen from './onboarding_screen';
+import ProfileScreen from './profile';
+import NotificationsScreen from './notifications';
+import SettingsScreen from './settings';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +17,9 @@ export default function AppNavigator() {
       <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
       <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Stack.Screen name="NotificationsScreen" component={NotificationsScreen} />
+      <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
       {/* Add other screens as needed */}
     </Stack.Navigator>
   );
